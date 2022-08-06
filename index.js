ff46897cd23e99cf356dd419f43c076343a81822
BarariuBeniamin <barariubeniamin@gmail.com>
2022-08-06 16:45:25 +0300

    extract get teamHTML

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
+function getTeamHTML(team) {
+  return `
+  <tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td> 
+      <a href="${team.url}">bbeni</a>
+    </td>
+    <td>x e</td>
+  </tr>`;
+}
+
 function displayTeams(teams) {
-  console.warn("display", teams);
+  // transforma in html
   var teamsHTML = "";
   teams.forEach(function (team) {
     console.info(team);
-    teamsHTML += `
-    <tbody>
-    <tr>
-      <td>${team.promotion}</td>
-      <td>${team.members}</td>
-      <td>${team.name}</td>
-      <td>
-        <a href="${team.url}">bbeni</a>
-      </td>
-      <td>x e</td>
-    </tr>
-  <tbody>`;
+    teamsHTML += getTeamHTML(team);
   });
+  // afisare
   document.querySelector("table tbody").innerHTML = teamsHTML;
 }
 
